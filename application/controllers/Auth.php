d888dfae0d4c6636b304036632f23f24bb333e51
Reinvy <satriaoner@gmail.com>
2022-03-02 02:10:39 +0700

    Membuat controller Auth

---
+++ application/controllers/Auth.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $data['title'] = 'Login';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registrasi()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[ami_user.email]', [
+            'is_unique' => 'Email sudah terdaftar!'
+        ]); //|is_unique[ami_user.email]
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[5]|matches[password2]', [
+            'matches' => 'Password dont match',
+            'min_length' => 'Password too short'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Registrasi';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registrasi');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->POST('name', true)),
+                'email' => htmlspecialchars($this->input->POST('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->POST('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('ami_user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation!</div>');
+            redirect('auth');
+        }
+    }
+}
