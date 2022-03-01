a38588595759984741431e76264b7a16c922181f
Reinvy <satriaoner@gmail.com>
2022-03-02 02:09:50 +0700

    Melakukan konfigurasi config

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,7 +76,7 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
 	'database' => 'siami',
 	'dbdriver' => 'mysqli',
