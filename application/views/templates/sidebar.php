6f1dd16c03662923f8f5275e6a71161c40f1c77b
Reinvy <satriaoner@gmail.com>
2022-03-10 14:01:53 +0700

    Membuat menu management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -13,36 +13,78 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
+    <!-- QueryMenu -->
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `ami_user_menu`.`id`, `menu`
+                  FROM `ami_user_menu` 
+                  JOIN `ami_user_access_menu` 
+                  ON `ami_user_menu`.`id` = `ami_user_access_menu`.`menu_id`
+                  WHERE `ami_user_access_menu`.`role_id` = $role_id
+                  ORDER BY `ami_user_access_menu`.`menu_id` ASC
+                  ";
+    $menu = $this->db->query($queryMenu)->result_array();
+    ?>
+
+    <!-- Looping Menu -->
     <!-- Heading -->
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+    <?php foreach ($menu as $m) : ?>
+        <div class="sidebar-heading">
+            <?= $m['menu'];  ?>
+        </div>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+        <!-- Sub Menu -->
+        <?php
+        // SELECT *
+        // FROM `ami_user_sub_menu` 
+        // WHERE `menu_id` = $menuId
+        // AND `is_active` = 1
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT *
+                         FROM `ami_user_sub_menu` 
+                         JOIN `ami_user_menu` 
+                         ON `ami_user_sub_menu`.`menu_id` = `ami_user_menu`.`id`
+                         WHERE `ami_user_sub_menu`.`menu_id` = $menuId
+                         AND `ami_user_sub_menu`.`is_active` = 1
+                         ";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+        <?php foreach ($subMenu as $sm) : ?>
 
-    <!-- Heading -->
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+            </li>
+
+        <?php endforeach; ?>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+
+    <?php endforeach; ?>
+
+
+
+
+
+    <!-- Heading
     <div class="sidebar-heading">
         User
     </div>
 
-    <!-- Nav Item - Charts -->
+    Nav Item - Charts
     <li class="nav-item">
         <a class="nav-link" href="charts.html">
             <i class="far fa-fw fa-user"></i>
             <span>My Profile</span></a>
-    </li>
+    </li> -->
 
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
+    <!-- <hr class="sidebar-divider"> -->
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
