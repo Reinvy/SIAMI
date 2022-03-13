6d70129ef9ba233b8a33962065af20441e3c7038
Reinvy <satriaoner@gmail.com>
2022-03-13 23:49:18 +0700

    membuat fitur load submenu dengan looping

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -50,27 +50,30 @@
         $subMenu = $this->db->query($querySubMenu)->result_array();
         ?>
 
+        <!-- Nav Item - Dashboard -->
         <?php foreach ($subMenu as $sm) : ?>
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <?php if ($title == $sm['title']) : ?>
+                <li class="nav-item active ">
+                <?php else : ?>
+                <li class="nav-item ">
+                <?php endif ?>
                 <a class="nav-link" href="<?= base_url($sm['url']); ?>">
                     <i class="<?= $sm['icon']; ?>"></i>
                     <span><?= $sm['title']; ?></span></a>
-            </li>
+                </li>
+
+            <?php endforeach; ?>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
 
         <?php endforeach; ?>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-    <?php endforeach; ?>
 
 
 
 
-
-    <!-- Heading
+        <!-- Heading
     <div class="sidebar-heading">
         User
     </div>
@@ -83,24 +86,24 @@
     </li> -->
 
 
-    <!-- Divider -->
-    <!-- <hr class="sidebar-divider"> -->
+        <!-- Divider -->
+        <!-- <hr class="sidebar-divider"> -->
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
+                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <span>Logout</span></a>
+        </li>
 
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
 </ul>
 <!-- End of Sidebar -->
