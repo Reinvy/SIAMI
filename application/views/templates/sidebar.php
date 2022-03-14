7fb0794579c9f745a4d51ff4722480d13503d0e6
Reinvy <satriaoner@gmail.com>
2022-03-14 16:40:37 +0700

    Memperbaiki sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -57,7 +57,7 @@
                 <?php else : ?>
                 <li class="nav-item ">
                 <?php endif ?>
-                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                <a class="nav-link pb-0" href="<?= base_url($sm['url']); ?>">
                     <i class="<?= $sm['icon']; ?>"></i>
                     <span><?= $sm['title']; ?></span></a>
                 </li>
@@ -65,7 +65,7 @@
             <?php endforeach; ?>
 
             <!-- Divider -->
-            <hr class="sidebar-divider">
+            <hr class="sidebar-divider mt-3">
 
         <?php endforeach; ?>
 
