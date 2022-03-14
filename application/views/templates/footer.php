343d3085feb331ba0318ec4d4c85fa8904ef99a5
Reinvy <satriaoner@gmail.com>
2022-03-14 16:40:53 +0700

    Menambahkan script ajax

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -48,6 +48,25 @@
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
+<script>
+    $('.form-check-input').on('click', function() {
+        const menuId = $(this).data('menu');
+        const roleId = $(this).data('role');
+
+        $.ajax({
+            url: "<?= base_url('admin/changeAccess'); ?>",
+            type: 'post',
+            data: {
+                menuId: menuId,
+                roleId: roleId
+            },
+            success: function() {
+                document.location.href = "<?= base_url('admin/roleAccess/'); ?>" + roleId;
+            }
+        });
+    })
+</script>
+
 </body>
 
 </html>
