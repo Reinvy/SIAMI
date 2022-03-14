bb93bf3992f2ca108a95b239281fbb64b2f9b635
Reinvy <satriaoner@gmail.com>
2022-03-14 16:38:38 +0700

    Menambahkan fungsi role, roleAccess, changeAccess

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,4 +21,54 @@ class Admin extends CI_Controller
         $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function role()
+    {
+        $data['title'] = 'Role';
+        $data['user'] = $this->db->get_where('ami_user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['role'] = $this->db->get('ami_user_role')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/role', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function roleAccess($role_id)
+    {
+        $data['title'] = 'Role Access';
+        $data['user'] = $this->db->get_where('ami_user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['role'] = $this->db->get_where('ami_user_role', ['id' => $role_id])->row_array();
+
+        $this->db->where('id !=', 1);
+        $data['menu'] = $this->db->get('ami_user_menu')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('admin/role-access', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function changeAccess()
+    {
+        $menu_id = $this->input->post('menuId');
+        $role_id = $this->input->post('roleId');
+
+        $data = [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+        ];
+
+        $result = $this->db->get_where('ami_user_access_menu', $data);
+
+        if ($result->num_rows() < 1) {
+            $this->db->insert('ami_user_access_menu', $data);
+        } else {
+            $this->db->delete('ami_user_access_menu', $data);
+        }
+    }
 }
