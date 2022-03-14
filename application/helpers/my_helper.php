ec24d0b85cc923382fcdb6c22ce0d0bac44bbd38
Reinvy <satriaoner@gmail.com>
2022-03-14 16:39:38 +0700

    Memperbaiki bug dan menambah fungsi check_access

--- application/helpers/my_helper.php
+++ application/helpers/my_helper.php
@@ -7,7 +7,7 @@ function is_logged_in()
     if (!$ci->session->userdata('email')) {
         redirect('auth');
     } else {
-        $role_id = $ci->session->userdata('role_admin');
+        $role_id = $ci->session->userdata('role_id');
         $menu = $ci->uri->segment(1);
 
         $queryMenu = $ci->db->get_where('ami_user_menu', ['menu' => $menu])->row_array();
@@ -19,5 +19,18 @@ function is_logged_in()
         if ($userAccess->num_rows() < 1) {
             redirect('auth/blocked');
         }
+    }
+}
+
+function check_access($role_id, $menu_id)
+{
+    $ci = get_instance();
+
+    $ci->db->WHERE('role_id', $role_id);
+    $ci->db->WHERE('menu_id', $menu_id);
+    $result = $ci->db->get('ami_user_access_menu');
+
+    if ($result->num_rows() > 0) {
+        return "checked='checked'";
     }
 }
